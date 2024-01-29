2aee95dd5334bb083bf0b2f89425aa37495d4ab7
cimafrancamae <cimafrancamae@gmail.com>
2024-01-29 20:51:48 +0800

    Add second database activities

--- Database/main.sql
+++ Database/main.sql
@@ -9,12 +9,12 @@ CREATE TABLE students (
 
 INSERT INTO students (id, first_name, last_name, age, location) 
 VALUES 
-    (1, 'Sheldon', 'Cooper', 27, 'Pasadena'),
-    (2, 'Leonard', 'Hofstadter', 26, 'Pasadena'),
-    (3, 'Penny', 'Teller', 22, 'Pasadena'),
-    (4, 'Howard', 'Wolowitz', 25, 'Pasadena'),
-    (5, 'Rajesh', 'Koothrappali', 26, 'Pasadena'),
-    (6, 'Bernadette', 'Rostenkowski', 22, 'Pasadena');
+    (1, 'Juan', 'Cruz', 18, 'Manila'),
+    (2, 'Anne', 'Wall', 20, 'Manila'),
+    (3, 'Theresa', 'Joseph', 21, 'Manila'),
+    (4, 'Isaac', 'Gray', 19, 'Laguna'),
+    (5, 'Zach', 'Matthews', 22, 'Marikina'),
+    (6, 'Finn', 'Lam', 25, 'Manila');
 
 UPDATE students 
 SET 
@@ -27,3 +27,13 @@ WHERE id = 1;
 
 
 DELETE FROM students WHERE id = 6;
+
+SELECT COUNT(*) AS total_students FROM students;
+
+SELECT * FROM students WHERE location = 'Manila';
+
+SELECT AVG(age) AS average_age FROM students;
+
+SELECT * FROM students ORDER BY age DESC;
+
+
