89902ec802c3250f146147637646f7a2299a918a
cimafrancamae <cimafrancamae@gmail.com>
2024-01-31 21:08:42 +0800

    Add Join activities

--- Database/main.sql
+++ Database/main.sql
@@ -36,4 +36,38 @@ SELECT ROUND(AVG(age),2) AS average_age FROM students;
 
 SELECT * FROM students ORDER BY age DESC;
 
+-- JOINS
+
+CREATE TYPE valid_grade AS ENUM('A','B','C','D','E','F');
+
+CREATE TABLE research_papers(
+    id SERIAL PRIMARY KEY,
+    student_id INTEGER NOT NULL,
+    grade VALID_GRADE,
+    FOREIGN KEY (student_id) REFERENCES students(id)
+);
+
+INSERT INTO research_papers(student_id, grade)
+VALUES
+    (1, 'A'),
+    (1, 'F'),
+    (1, 'B'),
+    (2, NULL),
+    (2, 'D'),
+    (2, NULL),
+    (3, 'A'),
+    (4, 'C'),
+    (5, 'E'),
+    (6, 'B');
+
+SELECT s.first_name, s.last_name, COUNT(r.*) AS paper_count
+FROM students AS s
+INNER JOIN research_papers AS r ON s.id = r.student_id
+GROUP BY s.id, s.first_name, s.last_name
+HAVING COUNT(r.*) > 1;
+
+SELECT s.first_name, s.last_name, r.id AS research_paper_id, r.grade 
+FROM students AS s
+INNER JOIN research_papers AS r ON s.id = r.student_id
+WHERE r.grade IS NULL;
 
