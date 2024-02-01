8db3401469532b77ca8cb5977ef2ef67712d7b69
cimafrancamae <cimafrancamae@gmail.com>
2024-02-01 21:03:42 +0800

    Replace is_a? with instance_of?

--- unique_in_order.rb
+++ unique_in_order.rb
@@ -3,7 +3,7 @@ def unique_in_order(sequence)
   unique_list = []
 
   for i in 0...sequence.length
-    current_item = sequence.is_a?(String) ? sequence[i] : sequence[i].to_s
+    current_item = sequence.instance_of?(String) ? sequence[i] : sequence[i].to_s
     previous_item = sequence[i - 1].to_s if i > 0
 
     if i == 0 || current_item != previous_item
@@ -18,9 +18,6 @@ end
   puts unique_in_order("AAAABBBCCDAABBB")
   puts unique_in_order("ABBCcAD")
   puts unique_in_order([1,2,2,3,3])
-  
-
-
 
 
 
