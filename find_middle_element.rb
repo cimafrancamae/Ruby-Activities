8fc943746093ceeb75dfeefd674005398a777ebd
cimafrancamae <cimafrancamae@gmail.com>
2024-03-26 20:16:18 +0800

    update find middle element

--- find_middle_element.rb
+++ find_middle_element.rb
@@ -6,12 +6,12 @@ def find_middle_element(arr)
 
   index = arr.index(middle)
 
-  puts "#{middle} is the number that fits between #{arr.min} and #{arr.max} and the index of  #{middle} in the input array is #{index}"
+  "#{middle} is the number that fits between #{arr.min} and #{arr.max} and the index of  #{middle} in the input array is #{index}"
 
 end
 
 arr = [2,3,1]
-find_middle_element(arr)
+puts find_middle_element(arr)
 
 arr = [5,10,14]
-find_middle_element(arr)
+puts find_middle_element(arr)
